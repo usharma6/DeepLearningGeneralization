01b00255ca443212ad137cf6d5e23473de25c05b
Udit Sharma <usharma6@jhu.edu>
2020-12-07 19:36:43 -0500

    python trianing files and instructions

---
+++ data.py
@@ -0,0 +1,140 @@
+import os
+import torch
+import torchvision.datasets as dataset
+import torchvision.transforms as transforms
+
+def AugmentedImageNet(train_transforms=[], val_transforms=[], test_transforms=[], batch_size=100):
+  
+    data_transforms = {
+        'train': transforms.Compose(
+            train_transforms
+        ),
+        'val': transforms.Compose(
+            val_transforms
+        ),
+        'test': transforms.Compose(
+            test_transforms
+        )
+    }
+
+    data_dir = 'tiny-imagenet-200/'
+    num_workers = {
+        'train' : 100,
+        'val'   : 0,
+        'test'  : 0
+    }
+    image_datasets = {x: dataset.ImageFolder(os.path.join(data_dir, x),
+                                              data_transforms[x])
+                      for x in ['train', 'val','test']}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=100,
+                                                 shuffle=True, pin_memory =True)
+                  for x in ['train', 'val', 'test']}
+
+    return dataloaders
+
+DATA_DICT = {
+    "Baseline_Blur" : AugmentedImageNet(train_transforms=[   
+      transforms.ToTensor(),
+      ], 
+    val_transforms=[
+      transforms.ToTensor(),
+      transforms.GaussianBlur(3, sigma=1.0)
+    ],
+    test_transforms=[]),
+
+    "Baseline_Rotation" : AugmentedImageNet(train_transforms=[   
+      transforms.ToTensor(),
+      ],  
+    val_transforms=[
+      transforms.ToTensor(),
+      transforms.RandomRotation(10),
+    ],
+    test_transforms=[]),
+
+    "Baseline_Jitter" : AugmentedImageNet(train_transforms=[   
+      transforms.ToTensor(),
+      ],  
+    val_transforms=[
+      transforms.ColorJitter(),   
+      transforms.ToTensor(),
+    ],
+    test_transforms=[]), 
+
+    "Baseline_Grayscale" : AugmentedImageNet(train_transforms=[   
+      transforms.ToTensor(),
+      ],  
+    val_transforms=[
+      transforms.ToTensor(),
+      transforms.RandomGrayscale(1), 
+    ],
+    test_transforms=[]),
+
+    "Baseline_Erasing" : AugmentedImageNet(train_transforms=[   
+      transforms.ToTensor(),
+      ], 
+    val_transforms=[
+      transforms.ToTensor(),
+      transforms.RandomErasing(p=1), 
+    ],
+    test_transforms=[]), 
+  
+    "Blur" : AugmentedImageNet(train_transforms=[  
+      transforms.RandomApply(torch.nn.ModuleList([transforms.ColorJitter()]), p=0.1),   
+      transforms.ToTensor(),                                                                             
+      transforms.RandomGrayscale(0.1),
+      transforms.RandomApply(torch.nn.ModuleList([transforms.RandomRotation(10)]), p=0.1),
+      transforms.RandomErasing(p=0.1)], 
+    val_transforms=[
+      transforms.ToTensor(),
+      transforms.GaussianBlur(3, sigma=1.0)
+    ],
+    test_transforms=[]),
+
+    "Rotation" : AugmentedImageNet(train_transforms=[  
+      transforms.RandomApply(torch.nn.ModuleList([transforms.ColorJitter()]), p=0.1),   
+      transforms.ToTensor(),                                                                             
+      transforms.RandomGrayscale(0.1),
+      transforms.RandomApply(torch.nn.ModuleList([transforms.GaussianBlur(3, sigma=1.0)]), p=0.1),
+      transforms.RandomErasing(p=0.1)], 
+    val_transforms=[
+      transforms.ToTensor(),
+      transforms.RandomRotation(10),
+    ],
+    test_transforms=[]),
+
+    "Jitter" : AugmentedImageNet(train_transforms=[  
+      transforms.ToTensor(),
+      transforms.RandomApply(torch.nn.ModuleList([transforms.RandomRotation(10)]), p=0.1),                                                                             
+      transforms.RandomGrayscale(0.1),
+      transforms.RandomApply(torch.nn.ModuleList([transforms.GaussianBlur(3, sigma=1.0)]), p=0.1),
+      transforms.RandomErasing(p=0.1)], 
+    val_transforms=[
+      transforms.ColorJitter(),   
+      transforms.ToTensor(),
+    ],
+    test_transforms=[]), 
+
+    "Grayscale" : AugmentedImageNet(train_transforms=[  
+      transforms.ToTensor(),
+      transforms.RandomApply(torch.nn.ModuleList([transforms.RandomRotation(10)]), p=0.1),                                                                             
+      transforms.RandomApply(torch.nn.ModuleList([transforms.ColorJitter()]), p=0.1),
+      transforms.RandomApply(torch.nn.ModuleList([transforms.GaussianBlur(3, sigma=1.0)]), p=0.1),
+      transforms.RandomErasing(p=0.1)], 
+    val_transforms=[
+      transforms.ToTensor(),
+      transforms.RandomGrayscale(1), 
+    ],
+    test_transforms=[]),
+
+    "Erasing" : AugmentedImageNet(train_transforms=[  
+      transforms.ToTensor(),
+      transforms.RandomApply(torch.nn.ModuleList([transforms.RandomRotation(10)]), p=0.1),                                                                             
+      transforms.RandomApply(torch.nn.ModuleList([transforms.ColorJitter()]), p=0.1),
+      transforms.RandomApply(torch.nn.ModuleList([transforms.GaussianBlur(3, sigma=1.0)]), p=0.1),
+      transforms.RandomGrayscale(0.1)],
+    val_transforms=[
+      transforms.ToTensor(),
+      transforms.RandomErasing(p=1), 
+    ],
+    test_transforms=[]), 
+}
---
+++ training.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+import torch
+import pickle
+import torch.nn as nn
+import torchvision
+import torchvision.datasets as dataset
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import time
+import struct
+import PIL
+import torchvision.models as models
+from data import DATA_DICT
+from model_eval import trainer
+
+def get_model(model_name, mode):
+  if model_name == "vgg":
+    model = models.vgg19_bn(pretrained=True)
+    model.classifier[-1] = nn.Linear(in_features=4096, out_features=200)
+    if mode == "augmented":
+      return model
+    elif mode == "baseline":
+      for param in model.parameters():
+        param.requires_grad = False
+      for param in model.classifier[-1].parameters():
+        param.requires_grad = True
+      return model
+  elif model_name == "googlenet":
+    model = models.googlenet(pretrained=True)
+    model.fc.out_features = 200
+    if mode == "augmented":
+      return model
+    elif mode == "baseline":
+      for param in model.parameters():
+        param.requires_grad = False
+      for param in model.fc.parameters():
+        param.requires_grad = True
+      return model
+  elif model_name == "resnet":
+    model = models.resnet18(pretrained=True)
+    model.avgpool = nn.AdaptiveAvgPool2d(1)
+    model.fc.out_features = 200
+    if mode == "augmented":
+      return model
+    elif mode == "baseline":
+      for param in model.parameters():
+        param.requires_grad = False
+      for param in model.fc.parameters():
+        requires_grad = True
+      return model
+
+def train_experiment(model, model_name, experiment, gpu_boole):
+  optimizer = torch.optim.Adam(model.parameters(), 0.001)
+  loss_metric = torch.nn.CrossEntropyLoss()
+  if gpu_boole:
+    model = model.cuda()
+  data = DATA_DICT[experiment]
+  train_runner = trainer(model, data, optimizer, loss_metric, 25, gpu_boole, 1, 1, 1, 0)
+  train_acc, epoch_loss, _, validation_acc,_ = train_runner.train_model()
+
+  results_pickle = {'train_acc': train_acc,
+                       'epoch_loss': epoch_loss,
+                       'val_acc': validation_acc}
+
+  pickle.dump(results_pickle, open('results/' + model_name + '/' + experiment +  '.pkl', 'wb'))
+  torch.save(model.state_dict(), 'models/' + model_name + '/' + experiment + '.model')
+
+def training(model_name, experiment_list, mode):
+  for experiment in experiment_list:
+    model = get_model(model_name, mode)
+    train_experiment(model, model_name, experiment, gpu_boole)
+
+if __name__ == "__main__":
+  model_list = ["vgg", "googlenet", "resnet"]
+  baseline_experiment_list = ["Baseline_Blur", "Baseline_Rotation", "Baseline_Jitter", "Baseline_Grayscale", "Baseline_Erasing"]
+  augmented_experiment_list = ["Blur", "Rotation", "Jitter", "Grayscale", "Erasing"]
+  experiment_list = [baseline_experiment_list, augmented_experiment_list]
+  mode_list = ["baseline", "augmented"]
+  for model in model_list:
+      for i in range(len(mode_list)):
+          training(model, experiment_list[i], mode_list[i])
